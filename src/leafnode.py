2ce013cf241cfa97cf38610b4c5e31ea096b9cfc
interyx <jtluensmann@gmail.com>
2024-10-10 17:49:26 -0400

    Added LeafNode and tests

---
+++ src/leafnode.py
@@ -0,0 +1,20 @@
+from htmlnode import HTMLNode
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("All leaf nodes must have a value")
+
+        result = self.value
+        props = ""
+        if self.props and self.tag:
+            props += super().props_to_html()
+            result = f"<{self.tag} {props}>{result}</{self.tag}>"
+        elif self.tag and not self.props:
+            result = f"<{self.tag}>{result}</{self.tag}>"
+        return result
+
+
---
+++ src/test_leafnode.py
@@ -0,0 +1,30 @@
+import unittest
+
+from leafnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+
+    def testChild(self):
+        node = LeafNode("p", "this is a paragraph of text")
+        self.assertIsNone(node.children)
+
+    def testTag(self):
+        node = LeafNode("p", "This is a paragraph of text")
+        result = '<p>This is a paragraph of text</p>'
+        self.assertEqual(node.to_html(), result)
+
+    def testNoTag(self):
+        node = LeafNode(tag=None, value="I am raw text!", props={"href": "https://www.google.com"})
+        result = 'I am raw text!'
+        self.assertEqual(node.to_html(), result)
+
+    def testValue(self):
+        node = LeafNode(tag = None, value = None, props = None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def testProps(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        result = '<a href="https://www.google.com">Click me!</a>'
+        self.assertEqual(node.to_html(), result)
+
