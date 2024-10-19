f15fe2d4618f8d1c2fee023b8642dbde6d4cfbf1
Joe Luensmann <jtluensmann@gmail.com>
2024-10-19 14:18:10 -0400

    Finished block identifier

--- src/mdutil.py
+++ src/mdutil.py
@@ -3,5 +3,36 @@ from src.constants import *
 def block_to_block_type(markdown):
     match(markdown[0]):
         case "#":
-            return MD_TYPE_H
+            pounds = markdown.count("#")
+            if 1 <= pounds <= 6:
+                return MD_TYPE_H
+        case "`":
+            if markdown[:3] == "```" and markdown[:3] == markdown[-3:]:
+                return MD_TYPE_CODE
+        case ">":
+            quotes = markdown.split("\n")
+            for line in quotes:
+                if line[0] != ">":
+                    return MD_TYPE_P
+            return MD_TYPE_QUOTE
+        case "*":
+            unordered_list = markdown.split("\n")
+            for line in unordered_list:
+                if line[0:2] != "* ":
+                    return MD_TYPE_P
+            return MD_TYPE_UL
+        case "-":
+            unordered_list = markdown.split("\n")
+            for line in unordered_list:
+                if line[0:2] != "- ":
+                    return MD_TYPE_P
+            return MD_TYPE_UL
+        case "1":
+            ordered_list = markdown.split("\n")
+            for idx, line in enumerate(ordered_list):
+                if line[0:3] != f"{idx+1}. ":
+                    return MD_TYPE_P
+            return MD_TYPE_OL
+                
 
+    return MD_TYPE_P
--- test/test_md_block_type.py
+++ test/test_md_block_type.py
@@ -15,3 +15,75 @@ class TestMDBlocks(unittest.TestCase):
     def test_h7(self):
         text = "####### This is a heading"
         self.assertEqual(MD_TYPE_P, block_to_block_type(text))
+
+    def test_codeblock(self):
+        text = "```This is a code block```"
+        self.assertEqual(MD_TYPE_CODE, block_to_block_type(text))
+
+    def test_codeblock_wrong1(self):
+        text = "``This is a code block```"
+        self.assertEqual(MD_TYPE_P, block_to_block_type(text))
+
+    def test_codeblock_wrong2(self):
+        text = "```This is a code block``"
+        self.assertEqual(MD_TYPE_P, block_to_block_type(text))
+
+    def test_quote(self):
+        text = "> I'm a quote"
+        self.assertEqual(MD_TYPE_QUOTE, block_to_block_type(text))
+
+    def test_quote_wrong1(self):
+        text = "> I'm a quote\nBut I'm invalid markdown"
+        self.assertEqual(MD_TYPE_P, block_to_block_type(text))
+
+    def test_three_quotes(self):
+        text = "> I'm a quote\n> Second quote line\n> Third quote line"
+        self.assertEqual(MD_TYPE_QUOTE, block_to_block_type(text))
+
+    def test_ul_one_item(self):
+        text = "* I'm a list with one line"
+        self.assertEqual(MD_TYPE_UL, block_to_block_type(text))
+
+    def test_ul_one_item_wrong(self):
+        text = "*I'm a list but I'm written wrong"
+        self.assertEqual(MD_TYPE_P, block_to_block_type(text))
+
+    def test_unordered_list(self):
+        text = "* I'm a valid list\n* I have asterisks and a space"
+        self.assertEqual(MD_TYPE_UL, block_to_block_type(text))
+        
+    def test_ul_second_line(self):
+        text = "* I'm a valid list\nBut the second line is wrong"
+        self.assertEqual(MD_TYPE_P, block_to_block_type(text))
+
+    def test_ul_dash(self):
+        text = "- I'm a valid list"
+        self.assertEqual(MD_TYPE_UL, block_to_block_type(text))
+
+    def test_ul_dash_wrong(self):
+        text = "-I'm an invalid list"
+        self.assertEqual(MD_TYPE_P, block_to_block_type(text))
+
+    def test_two_dash_ul(self):
+        text = "- I'm a valid list\nBut I have a second wrong item"
+        self.assertEqual(MD_TYPE_P, block_to_block_type(text))
+
+    def test_two_dash_ul_correct(self):
+        text = "- I'm a valid list\n- With a second valid item"
+        self.assertEqual(MD_TYPE_UL, block_to_block_type(text))
+
+    def test_ordered_list(self):
+        text = "1. I'm the first item in a list"
+        self.assertEqual(MD_TYPE_OL, block_to_block_type(text))
+
+    def test_ordered_list_wrong_format(self):
+        text = "1. I'm the first item in a list\n2 I'm the second list item"
+        self.assertEqual(MD_TYPE_P, block_to_block_type(text))
+
+    def test_ordered_list_wrong_order(self):
+        text = "1. I'm the first item\n3. I'm the second item\n2. I'm the third item"
+        self.assertEqual(MD_TYPE_P, block_to_block_type(text))
+
+    def test_ordered_list_correct_order(self):
+        text = "1. One\n2. Two\n3. Three"
+        self.assertEqual(MD_TYPE_OL, block_to_block_type(text))
