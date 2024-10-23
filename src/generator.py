35182fc9585b058284ac60c0d03df4092b2c79c2
interyx <jtluensmann@gmail.com>
2024-10-22 20:07:50 -0400

    Markdown -> blocks -> html function working

--- src/generator.py
+++ src/generator.py
@@ -5,6 +5,7 @@ from src.parentnode import ParentNode
 from src.util import *
 from src.mdutil import *
 
+
 def text_to_children(text):
     nodes = text_to_textnodes(text)
     text_nodes = []
@@ -12,15 +13,21 @@ def text_to_children(text):
         text_nodes.append(text_node_to_html_node(node))
     return text_nodes
 
+
 def process_paragraph(text):
     text_nodes = text_to_children(text)
-        
+
     return ParentNode(tag="p", children=text_nodes)
 
-def process_heading(text, level):
+
+def process_heading(text):
+    level = text.count("#")
+    text = filter(lambda x: x != "#", text)
+    text = "".join(text).strip()
     text_nodes = text_to_children(text)
 
     return ParentNode(tag=f"h{level}", children=text_nodes)
+
 
 def process_code(text):
     text = text.replace("```", "")
@@ -28,12 +35,14 @@ def process_code(text):
     code_block = ParentNode(tag="code", children=[node])
     return ParentNode(tag="pre", children=[code_block])
 
+
 def process_quote(text):
     text = text.replace("> ", "")
     nodes = text_to_children(text)
 
     return ParentNode(tag="blockquote", children=nodes)
-    
+
+
 def process_unordered_list(text):
     lines = text.replace("- ", "").replace("* ", "").split("\n")
     ul = []
@@ -41,6 +50,7 @@ def process_unordered_list(text):
         child_nodes = text_to_children(line)
         ul.append(ParentNode(tag="li", children=child_nodes))
     return ParentNode(tag="ul", children=ul)
+
 
 def process_ordered_list(text):
     lines = text.split("\n")
@@ -51,6 +61,7 @@ def process_ordered_list(text):
         final_list.append(ParentNode(tag="li", children=child_nodes))
     return ParentNode(tag="ol", children=final_list)
 
+
 def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
     nodes = []
@@ -60,4 +71,12 @@ def markdown_to_html_node(markdown):
             nodes.append(process_paragraph(block))
         if block_type == MD_TYPE_CODE:
             nodes.append(process_code(block))
+        if block_type == MD_TYPE_QUOTE:
+            nodes.append(process_quote(block))
+        if block_type == MD_TYPE_UL:
+            nodes.append(process_unordered_list(block))
+        if block_type == MD_TYPE_OL:
+            nodes.append(process_ordered_list(block))
+        if block_type == MD_TYPE_H:
+            nodes.append(process_heading(block))
     return ParentNode(tag="div", children=nodes)
--- test/test_generator.py
+++ test/test_generator.py
@@ -1,6 +1,7 @@
 import unittest
 from src.generator import *
 
+
 class TestGenerator(unittest.TestCase):
 
     def test_paragraph(self):
@@ -14,14 +15,16 @@ class TestGenerator(unittest.TestCase):
         self.assertEqual(result, process_paragraph(text).to_html())
 
     def test_heading(self):
-        text = "This is a level 1 heading"
+        text = "# This is a level 1 heading"
         result = "<h1>This is a level 1 heading</h1>"
-        self.assertEqual(result, process_heading(text, 1).to_html())
+        self.assertEqual(result, process_heading(text).to_html())
 
     def test_heading_styles(self):
-        text = "This is a level 4 heading with **bold** text for some reason"
-        result = "<h4>This is a level 4 heading with <b>bold</b> text for some reason</h4>"
-        self.assertEqual(result, process_heading(text, 4).to_html())
+        text = "#### This is a level 4 heading with **bold** text for some reason"
+        result = (
+            "<h4>This is a level 4 heading with <b>bold</b> text for some reason</h4>"
+        )
+        self.assertEqual(result, process_heading(text).to_html())
 
     def test_code_block(self):
         text = """```#include <stdio.h>
@@ -29,17 +32,17 @@ int main() {
     print("Hello, world!");
     return 0;
 }```"""
-        result = "<pre><code>#include <stdio.h>\nint main() {\n    print(\"Hello, world!\");\n    return 0;\n}</code></pre>"
+        result = '<pre><code>#include <stdio.h>\nint main() {\n    print("Hello, world!");\n    return 0;\n}</code></pre>'
         self.assertEqual(result, process_code(text).to_html())
 
     def test_quotes(self):
-        text="> America is built on speed.\n> Hot, nasty, badass speed.\n> - Eleanor Roosevelt"
+        text = "> America is built on speed.\n> Hot, nasty, badass speed.\n> - Eleanor Roosevelt"
         result = "<blockquote>America is built on speed.\nHot, nasty, badass speed.\n- Eleanor Roosevelt</blockquote>"
         self.assertEqual(result, process_quote(text).to_html())
 
     def test_unordered_list(self):
         text = "- Frankenstein's Monster\n- Dracula\n- The Mummy\n- The Invisible Man"
-        result="<ul><li>Frankenstein's Monster</li><li>Dracula</li><li>The Mummy</li><li>The Invisible Man</li></ul>"
+        result = "<ul><li>Frankenstein's Monster</li><li>Dracula</li><li>The Mummy</li><li>The Invisible Man</li></ul>"
         self.assertEqual(result, process_unordered_list(text).to_html())
 
     def test_one_unordered_list(self):
@@ -54,9 +57,10 @@ int main() {
 
     def test_ordered_list(self):
         text = "1. Iron Man\n2. Captain America\n3. Incredible Hulk"
-        result = "<ol><li>Iron Man</li><li>Captain America</li><li>Incredible Hulk</li></ol>"
+        result = (
+            "<ol><li>Iron Man</li><li>Captain America</li><li>Incredible Hulk</li></ol>"
+        )
         self.assertEqual(result, process_ordered_list(text).to_html())
-
 
     def test_markdown_2html_simple(self):
         markdown = "This is a simple paragraph tag"
@@ -69,6 +73,31 @@ int main() {
         self.assertEqual(result, markdown_to_html_node(markdown).to_html())
 
     def test_markdown_2html_p_code(self):
-        markdown = "Paragraph block\n\n```print(\"Hello, World!\")```"
-        result = "<div><p>Paragraph block</p><pre><code>print(\"Hello, World!\")</code></pre></div>"
+        markdown = 'Paragraph block\n\n```print("Hello, World!")```'
+        result = '<div><p>Paragraph block</p><pre><code>print("Hello, World!")</code></pre></div>'
+        self.assertEqual(result, markdown_to_html_node(markdown).to_html())
+
+    def test_paragraph_quote(self):
+        markdown = "Paragraph block\nSecond line of paragraph\n\n> Four score and seven years ago,\n> America ran on Dunkin'"
+        result = "<div><p>Paragraph block\nSecond line of paragraph</p><blockquote>Four score and seven years ago,\nAmerica ran on Dunkin'</blockquote></div>"
+        self.assertEqual(result, markdown_to_html_node(markdown).to_html())
+
+    def test_quote_list(self):
+        markdown = "> What is a man?\n> A miserable little pile of secrets\n\n* Castlevania\n* Castlevania II: Simon's Quest\n* Castlevania III: A Different One"
+        result = "<div><blockquote>What is a man?\nA miserable little pile of secrets</blockquote><ul><li>Castlevania</li><li>Castlevania II: Simon's Quest</li><li>Castlevania III: A Different One</li></ul></div>"
+        self.assertEqual(result, markdown_to_html_node(markdown).to_html())
+
+    def test_code_ol(self):
+        markdown = "```it's some code in a block```\n\n1. A New Hope\n2. Empire Strikes Back\n3. Return of the Jedi"
+        result = "<div><pre><code>it's some code in a block</code></pre><ol><li>A New Hope</li><li>Empire Strikes Back</li><li>Return of the Jedi</li></ol></div>"
+        self.assertEqual(result, markdown_to_html_node(markdown).to_html())
+
+    def test_heading_paragraph(self):
+        markdown = "# Heading 1\n\nParagraph 1"
+        result = "<div><h1>Heading 1</h1><p>Paragraph 1</p></div>"
+        self.assertEqual(result, markdown_to_html_node(markdown).to_html())
+
+    def test_headings_paragraphs(self):
+        markdown = "## Heading 2\n\nParagraph 1\n\n#### Heading 4\n\nParagraph with **bold** and *italic* text"
+        result = "<div><h2>Heading 2</h2><p>Paragraph 1</p><h4>Heading 4</h4><p>Paragraph with <b>bold</b> and <i>italic</i> text</p></div>"
         self.assertEqual(result, markdown_to_html_node(markdown).to_html())
